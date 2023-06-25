65bb311c3b6a3f173cc514a9c9129d87b40afc9a
keiko233 <keiko23333@gmail.com>
2023-06-25 17:40:35 +0800

    Initial Code

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<template>
+  <h3>BeatSaver</h3>
+</template>
+
+<script setup lang="ts">
+</script>
+
+<style scoped lang="less">
+</style>
---
+++ vite.config.ts
@@ -0,0 +1,24 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    AutoImport({}),
+    Components({})
+  ],
+  build: {
+    assetsDir: "./",
+    rollupOptions: {
+      output: {
+        entryFileNames: `[name].js`,
+        chunkFileNames: `[name].js`,
+        assetFileNames: `[name].[ext]`,
+        format: 'umd'
+      }
+    }
+  }
+})
