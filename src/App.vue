4ee016f9baad793f3215b2e62216ae92e23ed37a
keiko233 <keiko23333@gmail.com>
2023-06-25 17:56:09 +0800

    feat: add documentation

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,26 @@
 <template>
   <h3>BeatSaver</h3>
+  <p>使用方法，安装就能用。</p>
+  <p>然后你的歌名旁边就会出现<BeatSaver style="display: inline-block; margin: 0;" />图标，点击它就可以快速到BeatSaver搜索了。</p>
 </template>
 
 <script setup lang="ts">
 </script>
 
 <style scoped lang="less">
+* {
+  color: #fff;
+  text-shadow: 0 0 4px rgb(0 0 0 / 33%);
+}
+
+h3 {
+  font-size: 36px;
+  font-weight: 700;
+  font-family: Arial;
+}
+
+p {
+  font-size: 16px;
+  line-height: 24px;
+}
 </style>
