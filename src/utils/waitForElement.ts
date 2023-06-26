cebd717d15a09d326565e4af8c5f8ba008db2122
keiko233 <keiko23333@gmail.com>
2023-06-26 14:55:30 +0800

    port waitForElement from 'elaina-theme-netease'

---
+++ src/utils/waitForElement.ts
@@ -0,0 +1,29 @@
+export const waitForElement = (selector: any, fun: any) => {
+  selector = selector.split(',');
+  let done = true;
+  for (const s of selector) {
+    if (!document.querySelector(s)) {
+      done = false;
+    }
+  }
+  if (done) {
+    for (const s of selector) {
+      fun.call(this, document.querySelector(s));
+    }
+    return;
+  }
+  let interval = setInterval(() => {
+    let done = true;
+    for (const s of selector) {
+      if (!document.querySelector(s)) {
+        done = false;
+      }
+    }
+    if (done) {
+      clearInterval(interval);
+      for (const s of selector) {
+        fun.call(this, document.querySelector(s));
+      }
+    }
+  }, 100);
+}
