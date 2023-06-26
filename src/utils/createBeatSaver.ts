d2f2e6bf8a743875e0b3f89bc0112206edf39f5d
keiko233 <keiko23333@gmail.com>
2023-06-26 14:56:08 +0800

    fix: inject Element when Element loaded

--- src/utils/createBeatSaver.ts
+++ src/utils/createBeatSaver.ts
@@ -1,26 +1,28 @@
 import { createApp, h } from 'vue'
 import BeatSaver from '../components/BeatSaver.vue'
+import { waitForElement } from './waitForElement';
 
 export const createBeatSaver = () => {
-  const targetElement = document.querySelector('.g-mn.one');
-  const observer = new MutationObserver(function () {
-    var tdColTitles = document.querySelectorAll('.td.col.title');
-    tdColTitles.forEach(function (tdColTitle) {
-      const existingDiv = tdColTitle.querySelector('.tit.s-fc1 + div');
-      const title = tdColTitle.querySelector('.tit')?.childNodes[0]?.nodeValue?.trim();
+  waitForElement('.g-mn.one', () => {
+    const targetElement = document.querySelector('.g-mn.one');
+    const observer = new MutationObserver(function (mutations) {
+      var tdColTitles = document.querySelectorAll('.td.col.title');
+      tdColTitles.forEach(function (tdColTitle) {
+        const existingDiv = tdColTitle.querySelector('.tit.s-fc1 + div');
+        const title = tdColTitle.querySelector('.tit')?.childNodes[0]?.nodeValue?.trim();
 
-      if (!existingDiv) {
-        const newDiv = document.createElement('div');
-        createApp({
-          render: () => h(BeatSaver, { title: title })
-        }).mount(newDiv);
-        const titSfc1 = tdColTitle.querySelector('.tit.s-fc1');
-        titSfc1?.insertAdjacentElement('afterend', newDiv);
-      }
+        if (!existingDiv) {
+          const newDiv = document.createElement('div');
+          createApp({
+            render: () => h(BeatSaver, { title: title })
+          }).mount(newDiv);
+          const titSfc1 = tdColTitle.querySelector('.tit.s-fc1');
+          titSfc1?.insertAdjacentElement('afterend', newDiv);
+        }
+      });
     });
 
+    // @ts-ignore
+    observer.observe(targetElement, { attributes: true, childList: true, subtree: true });
   });
-
-  // @ts-ignore
-  observer.observe(targetElement, { attributes: true, childList: true, subtree: true });
 }
